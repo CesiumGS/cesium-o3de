5acb5e1b4385768bea3557d12c95be013af37acd
Bao Tran <tranthienbao123@gmail.com>
2021-07-09 14:00:10 -0400

    WIP: fix tarball extension and add CesiumNative to gem

--- Gems/Cesium/External/PostInstall.cmake
+++ Gems/Cesium/External/PostInstall.cmake
@@ -24,9 +24,9 @@ file(WRITE ${PACKAGE_BUILD_DIR}/SHA256SUMS ${SHA256SUMS_CONTENT})
 
 # create tarball file for Packages/Install directory
 file(MAKE_DIRECTORY ${PACKAGE_INSTALL_DIR})
-execute_process(COMMAND ${CMAKE_COMMAND} -E tar "cfz" "${PACKAGE_INSTALL_DIR}/CesiumNative.tgz" "${PACKAGE_BUILD_DIR}"
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar "cJf" "${PACKAGE_INSTALL_DIR}/CesiumNative.tar.xz" "${PACKAGE_BUILD_DIR}"
                 WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
 
 # save tarball SHA256 in a file for o3de
-file(SHA256 ${PACKAGE_INSTALL_DIR}/CesiumNative.tgz TARBALL_CHECKSUM)
+file(SHA256 ${PACKAGE_INSTALL_DIR}/CesiumNative.tar.xz TARBALL_CHECKSUM)
 file(WRITE ${PACKAGE_INSTALL_DIR}/SHA256SUMS ${TARBALL_CHECKSUM})
