ec7a965917666ac2af551a57dbcdd3a5fc836949
Bao Tran <tranthienbao123@gmail.com>
2021-07-09 11:14:40 -0400

    WIP: produce Tarball for package directory

--- Gems/Cesium/External/PostInstall.cmake
+++ Gems/Cesium/External/PostInstall.cmake
@@ -1,10 +1,23 @@
+set(PACKAGE_BUILD_DIR ${CMAKE_CURRENT_LIST_DIR}/Packages)
+set(PACKAGE_INSTALL_DIR ${PACKAGE_DIR}/Install)
+
 # copy license file to Packages/CesiumNative/CesiumNative
 file(COPY 
       ${CMAKE_CURRENT_LIST_DIR}/CesiumNative/LICENSE 
-      DESTINATION ${CMAKE_CURRENT_LIST_DIR}/Packages/CesiumNative/CesiumNative/)
+      DESTINATION ${PACKAGE_BUILD_DIR}/CesiumNative/CesiumNative/)
 
 # generate SHA256 for files in Packages/CesiumNative
+file(GLOB_RECURSE PACKAGE_FILES 
+      LIST_DIRECTORIES false
+      ${PACKAGE_BUILD_DIR}/CesiumNative/*)
+
+foreach(FILE ${PACKAGE_FILES})
+
+endforeach()
 
 # create tarball file for Packages/CesiumNative directory
+file(MAKE_DIRECTORY ${PACKAGE_INSTALL_DIR})
+execute_process(COMMAND ${CMAKE_COMMAND} -E tar "cfvz" "${PACKAGE_INSTALL_DIR}/CesiumNative.tgz" "${PACKAGE_BUILD_DIR}/CesiumNative"
+                WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR})
 
 # save tarball SHA256 in a file for o3de
